4dd1bb8bed0b1659a369e8f034c8f9f33030bdb8
GitItHowYouLive <MicStandTerrence@gmail.com>
2022-05-03 17:06:42 -0500

    Initial Commit, Exercise Complete

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double feelsDifferent = 60;
+
+            Console.WriteLine($"Converting {feelsDifferent} degrees celcius to fahrenheit gives you " + TempConverter.CelsiusToFahrenheit(feelsDifferent));
+                     
+            Console.WriteLine($"Converting {feelsDifferent} degrees fahrenheit to Celcius, however, gives you " + TempConverter.FahrenheitToCelcius(feelsDifferent));
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+        public static double FahrenheitToCelcius(double temp)
+        {
+            return (temp - 32) / 1.8;
+        }
+
+        public static double CelsiusToFahrenheit(double temp)
+        {
+            return temp * 1.8 + 32;
+        }
+    }
+}
